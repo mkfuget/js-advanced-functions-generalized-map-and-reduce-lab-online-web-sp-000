482a6b91fbd6db4a1ff560bd4e7dbefe58d151df
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 06:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, func)
+{
+  out = [];
+  for(let i=0; i<array.length; i++)
+  {
+    out.push(func(array[i]))
+  }
+  return out;
+}
+
+function map(array, func)
+{
+  out = [];
+  for(let i=0; i<array.length; i++)
+  {
+    out.push(func(array[i]))
+  }
+  return out;
+}
