edd75d990191d5e6057e74f708d0494d48fdb1a4
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 06:24:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(array, func)
+{
+  let out = [];
+  for(let i=0; i<array.length; i++)
+  {
+    out.push(func(array[i]))
+  }
+  return out;
+}
+
+function reduce(array, func, startingPoint=0)
+{
+  let out = array[0];
+  for(let i=1; i<array.length; i++)
+  {
+    out = func(array[i], out)
+  }
+  if(startingPoint!= 0)
+  {
+    return out+startingPoint
+  }
+  else
+  {
+    return out
+  }
+}
